44c3df896d55a2888c04ed76547a3faa83c37e7f
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-11-04 16:56:26 +0700

    register with role

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', function () {
+    return view('login');
+});
 Route::get('/login', function () {
     return view('login');
 });
